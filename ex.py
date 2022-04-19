f5741319da43fb6a6aa8b62e0ec823ad324e4073
Bianca-Gabriela Dobre <bianca.dobre2407@stud.fils.upb.ro>
2022-04-19 11:07:09 +0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,8 +1,8 @@
 #ex 1
 def on_forever():
     if input.button_is_pressed(Button.B):
-        basic.show_number(input.temperature()) #display the temperature
-    elif input.logo_is_pressed(): #check the light level
+        basic.show_number(input.temperature()) 
+    elif input.logo_is_pressed(): 
         basic.show_icon(input.light_level())
     else:
         basic.clear_screen()
